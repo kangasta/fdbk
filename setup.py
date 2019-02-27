74dc0235c23579306c6ea14751cfde773c51e905
Toni Kangas <toni.ar.kangas@gmail.com>
2019-02-28 00:23:42 +0200

    v0.8.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 	name="fdbk",
-	version="0.8.4",
+	version="0.8.5",
 	author="Toni Kangas",
 	description="Backend and DB wrapper for feedback collection system",
 	long_description=long_description,
