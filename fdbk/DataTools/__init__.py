7bbc2800b66b3cac836d28b82d2ac86dd307dc3f
Toni Kangas <toni.ar.kangas@gmail.com>
2019-03-22 01:49:27 +0200

    Add module level docstring for DataTools

--- fdbk/DataTools/__init__.py
+++ fdbk/DataTools/__init__.py
@@ -1,2 +1,8 @@
+'''Data analysis tools
+
+Functions to ease the simple data analysis done by the DBConnection.
+
+'''
+
 from ._SummaryFuncs import SummaryFuncs
 from ._VisualizationFuncs import VisualizationFuncs
