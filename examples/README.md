a76efa774d95ee42d6810cfb2772017db447f374
Toni Kangas <toni.ar.kangas@gmail.com>
2019-03-21 21:46:55 +0200

    Add system status reporter example

--- examples/README.md
+++ examples/README.md
@@ -2,13 +2,19 @@
 
 ## Example for Reporter usage
 
-The `net_status.py` provides an example on how to use `fdbk.Reporter` to automatically push data from data source to fdbk. For example, run
+The `net_status.py` and `sys_status.py` provide examples on how to use `fdbk.Reporter` to automatically push data from data source to fdbk. For example, run
 
 ```bash
 python3 net_status.py -i 10 -n 5 -t Google https://google.com http://localhost:8080
 ```
 
-to poll elapsed time of get request to Google with 5 sample averaging over 10 seconds.
+to poll elapsed time of get request to Google with 5 sample averaging over 10 seconds or
+
+```bash
+python3 net_status.py -i 10 -n 5 http://localhost:8080
+```
+
+to poll system resource usage with same averaging. Both example commands push the data to fdbk server in port 8080 of localhost via fdbk ClientConnection.
 
 ## Example data for POST requests
 
---
+++ examples/sys_status.py
@@ -0,0 +1,62 @@
+import psutil
+
+from threading import Thread, RLock
+
+from requests import get
+from requests.exceptions import ReadTimeout
+
+class SysStatus(object):
+	def __init__(self, topic_name='System status'):
+		self.__topic_name = topic_name
+
+	@property
+	def topic(self):
+		fields = ['CPU_usage', 'memory_usage', 'disk_usage']
+		units = (
+			list(map(lambda field: {'field': field, 'unit': 'percent'}, fields))
+		)
+
+		return {
+			"name": self.__topic_name,
+			"type_str": "sysstatus",
+			"description": "System status monitor.",
+			"fields": fields,
+			"units": units,
+			"summary": list(map(lambda field: {
+				"field": field,
+				"method":"latest"
+			}, fields)),
+			"visualization": list(map(lambda field: {
+				"field": field,
+				"method":"line"
+			}, fields))
+		}
+
+	@property
+	def data(self):
+		data = {
+			'CPU_usage': psutil.cpu_percent(),
+			'memory_usage': psutil.virtual_memory().percent,
+			'disk_usage': psutil.disk_usage('/').percent
+		}
+
+		return data
+
+if __name__ == '__main__':
+	from argparse import ArgumentParser
+	import json
+
+	from fdbk import Reporter
+
+	parser = ArgumentParser()
+
+	parser.add_argument("db_parameters", nargs="+", type=str, help="Parameters for fdbk DB connection.")
+	parser.add_argument("--db-connection", type=str, default="ClientConnection", help="fdbk DB connection to use (default=ClientConnection)")
+	parser.add_argument("--interval", "-i", type=float, default=360.0, help="Data pushing interval in seconds.")
+	parser.add_argument("--num-samples", "-n", type=int, default=60, help="Number of samples to average during the push interval")
+	args = parser.parse_args()
+
+	SYS_STATUS = SysStatus()
+
+	REPORTER = Reporter(SYS_STATUS, args.db_connection, args.db_parameters)
+	REPORTER.start(interval=args.interval, num_samples=args.num_samples)
