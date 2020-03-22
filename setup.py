e589f98ba0d3ac15ca6e31e9e393642df5f6b528
Toni Kangas <toni.ar.kangas@gmail.com>
2020-03-23 00:31:48 +0200

    Use python3 in scripts

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setuptools.setup(
         "flask",
         "requests"
     ],
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    )
+    ]
 )
