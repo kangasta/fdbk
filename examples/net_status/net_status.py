265fa4e7d1e4f73366c84c02372a6de7e84d6229
Toni Kangas <toni.ar.kangas@gmail.com>
2020-03-29 16:36:58 +0300

    Rewrite net status to write all targets to separate topics

--- examples/net_status/net_status.py
+++ examples/net_status/net_status.py
@@ -1,107 +1,92 @@
 from datetime import timedelta
 
-from threading import Thread, RLock
-
 from requests import get
 from requests.exceptions import ReadTimeout
 
 class NetStatus(object):
     @staticmethod
-    def __validate_targets(targets):
-        for target in targets:
-            try:
-                assert type(target['name']) is str
-                assert type(target['url']) is str
-            except AssertionError:
-                raise ValueError('Invalid target: ' + str(target))
+    def _validate(target):
+        try:
+            assert type(target.get('name')) is str
+            assert type(target.get('url')) is str
+        except AssertionError:
+            raise ValueError('Invalid target: ' + str(target))
 
-    def __init__(self, targets, topic_name='Network status', timeout=2):
-        if type(targets) is not list or len(targets) == 0:
-            raise ValueError('No targets given')
+    def __init__(self, target, timeout=3):
+        self._validate(target)
 
-        self.__validate_targets(targets)
-
-        self.__targets = targets
-        self.__timeout = timeout
-        self.__topic_name = topic_name
+        self._target = target
+        self._timeout = timeout
 
     @property
     def topic(self):
-        target_names = [target['name'] for target in self.__targets]
-        fields = (
-            list(map(lambda name: name + '_elapsed', target_names)) +
-            list(map(lambda name: name + '_status_code', target_names))
-        )
-        units = (
-            list(map(lambda name: {
-                'field': name + '_elapsed',
-                'unit': 'ms'
-            }, target_names))
-        )
-
         return {
-            "name": self.__topic_name,
+            "name": self._target.get('name'),
             "type_str": "netstatus",
             "description": "Network connection status monitor.",
-            "fields": fields,
-            "units": units,
-            "summary": list(map(lambda field: {
-                "field": field,
-                "method":"latest"
-            }, (target['name'] + '_elapsed' for target in self.__targets))),
-            "visualization": list(map(lambda field: {
-                "field": field,
-                "method":"line"
-            }, (target['name'] + '_elapsed' for target in self.__targets)))
+            "fields": ['elapsed', 'status_code'],
+            "units": [{
+                'field': 'elapsed',
+                'unit': 'ms',
+            }],
+            "summary": [{
+                "field": 'elapsed',
+                "method": "latest",
+            }],
+            "visualization": [{
+                "field": 'elapsed',
+                "method": "line",
+            }],
         }
 
     @property
     def data(self):
-        lock = RLock()
         data = {}
 
-        def __run_get(name, url):
-            try:
-                response = get(url, timeout=self.__timeout)
-                with lock:
-                    data[name + '_elapsed'] = (response.elapsed / timedelta(microseconds=1)) / 1000.0
-                    data[name + '_status_code'] = response.status_code
-            except ReadTimeout:
-                with lock:
-                    data[name + '_elapsed'] = self.__timeout * 1000.0
-                    data[name + '_status_code'] = None
+        try:
+            response = get(self._target.get('url'), timeout=self._timeout)
+            data['elapsed'] = (response.elapsed / timedelta(microseconds=1)) / 1000.0
+            data['status_code'] = response.status_code
+        except ReadTimeout:
+            data[name + '_elapsed'] = self._timeout * 1000.0
+            data[name + '_status_code'] = None
+
+        return data
+
+if __name__ == '__main__':
+    def main():
+        parser = get_reporter_argparser()
+
+        parser.add_argument("--target", "-t", action="append", default=[], type=str, nargs=2, metavar=("name","url"), help="Add target to monitor.")
+        parser.add_argument("--target-file", "-f", type=str, help="Add targets from json file to monitor.")
+        args = parser.parse_args()
+
+        targets_cmd = [{'name': name, 'url': url} for name, url in args.target]
+        if args.target_file is not None:
+            with open(args.target_file) as f:
+                targets_file = json.load(f)
+        else:
+            targets_file = []
+
+        targets = targets_cmd + targets_file
+        data_sources = (NetStatus(target) for target in targets)
+
+        reporters = (Reporter(data_source, args.db_connection, args.db_parameters, verbose=args.verbose) for data_source in data_sources)
 
         threads = []
-        for target in self.__targets:
-            threads.append(Thread(target=__run_get, args=[target['name'], target['url']]))
+        for reporter in reporters:
+            threads.append(Thread(target=reporter.start, kwargs=dict(interval=args.interval, num_samples=args.num_samples)))
             threads[-1].start()
 
         for thread in threads:
             thread.join()
 
-        return data
 
-if __name__ == '__main__':
     from argparse import ArgumentParser
     import json
+    from threading import Thread
 
     from fdbk import Reporter
     from fdbk.utils import get_reporter_argparser
 
-    parser = get_reporter_argparser()
-
-    parser.add_argument("--target", "-t", action="append", default=[], type=str, nargs=2, metavar=("name","url"), help="Add target to monitor.")
-    parser.add_argument("--target-file", "-f", type=str, help="Add targets from json file to monitor.")
-    args = parser.parse_args()
-
-    targets_cmd = [{'name': name, 'url': url} for name,url in args.target]
-    if args.target_file is not None:
-        with open(args.target_file) as f:
-            targets_file = json.load(f)
-    else:
-        targets_file = []
-
-    NET_STATUS = NetStatus(targets_cmd + targets_file)
-
-    REPORTER = Reporter(NET_STATUS, args.db_connection, args.db_parameters, verbose=args.verbose)
-    REPORTER.start(interval=args.interval, num_samples=args.num_samples)
+    main()
