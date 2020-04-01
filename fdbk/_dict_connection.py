b68ec417ac32fa5e6d6b196b27d88462ee2332bc
Toni Kangas <toni.ar.kangas@gmail.com>
2020-04-01 22:19:03 +0300

    Move static methods from DBConnection to utils

--- fdbk/_dict_connection.py
+++ fdbk/_dict_connection.py
@@ -2,6 +2,12 @@ from os.path import expanduser
 import json
 
 from fdbk import DBConnection
+from fdbk.utils import (
+    generate_data_entry,
+    generate_data_response,
+    generate_topic_dict,
+    generate_topic_response,
+    generate_topics_list)
 
 
 class DictConnection(DBConnection):
@@ -21,7 +27,7 @@ class DictConnection(DBConnection):
         }
 
     def add_topic(self, name, **kwargs):
-        topic_d = DBConnection.generate_topic_dict(name, add_id=True, **kwargs)
+        topic_d = generate_topic_dict(name, add_id=True, **kwargs)
 
         self.__dict["topics"].append(topic_d)
         self.__dict[topic_d["id"]] = []
@@ -41,12 +47,12 @@ class DictConnection(DBConnection):
                            "' not found from database")
         fields = topic_d["fields"]
 
-        data = DBConnection.generate_data_entry(topic_id, fields, values)
+        data = generate_data_entry(topic_id, fields, values)
         self.__dict[topic_id].append(data)
 
     def get_topics(self):
         topics = self.__dict["topics"]
-        return DBConnection.generate_topics_list(topics)
+        return generate_topics_list(topics)
 
     def get_topic(self, topic_id):
         try:
@@ -56,7 +62,7 @@ class DictConnection(DBConnection):
             raise KeyError("Topic ID '" + topic_id +
                            "' not found from database")
 
-        return DBConnection.generate_topic_response(topic)
+        return generate_topic_response(topic)
 
     def get_data(self, topic_id):
         try:
@@ -68,7 +74,7 @@ class DictConnection(DBConnection):
         fields = topic_d["fields"]
         data = self.__dict[topic_id]
 
-        return DBConnection.generate_data_response(data, fields)
+        return generate_data_response(data, fields)
 
 
 ConnectionClass = DictConnection
