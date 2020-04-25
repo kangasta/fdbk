2b431959a015bbc0d6514e88cb9b392e695569dc
Toni Kangas <toni.ar.kangas@gmail.com>
2020-04-25 23:29:03 +0300

    Add test for create_db_connection

--- fdbk/_dict_connection.py
+++ fdbk/_dict_connection.py
@@ -19,45 +19,45 @@ class DictConnection(DBConnection):
     '''
 
     def __init__(self, topics_db_backup=None):
-        self.__topics_backup = topics_db_backup
+        self._topics_backup = topics_db_backup
         topics = []
 
-        if self.__topics_backup:
+        if self._topics_backup:
             try:
-                with open(expanduser(self.__topics_backup), 'r') as f:
+                with open(expanduser(self._topics_backup), 'r') as f:
                     topics = json.load(f)['topics']
             except FileNotFoundError:
                 pass
 
-        self.__dict = {
+        self._dict = {
             "topics": topics
         }
 
     def add_topic(self, name, **kwargs):
         topic_d = generate_topic_dict(name, add_id=True, **kwargs)
 
-        self.__dict["topics"].append(topic_d)
-        self.__dict[topic_d["id"]] = []
+        self._dict["topics"].append(topic_d)
+        self._dict[topic_d["id"]] = []
 
-        if self.__topics_backup:
-            with open(expanduser(self.__topics_backup), 'w') as f:
-                json.dump({'topics': self.__dict["topics"]}, f)
+        if self._topics_backup:
+            with open(expanduser(self._topics_backup), 'w') as f:
+                json.dump({'topics': self._dict["topics"]}, f)
 
         return topic_d["id"]
 
     def add_data(self, topic_id, values):
         try:
             topic_d = next(
-                i for i in self.__dict["topics"] if i["id"] == topic_id)
+                i for i in self._dict["topics"] if i["id"] == topic_id)
         except StopIteration:
             raise KeyError(topic_not_found(topic_id))
         fields = topic_d["fields"]
 
         data = generate_data_entry(topic_id, fields, values)
-        self.__dict[topic_id].append(data)
+        self._dict[topic_id].append(data)
 
     def get_topics(self, type_=None):
-        topics = self.__dict["topics"]
+        topics = self._dict["topics"]
         if type_:
             topics = [i for i in topics if i.get('type') == type_]
 
@@ -66,7 +66,7 @@ class DictConnection(DBConnection):
     def get_topic(self, topic_id):
         try:
             topic = next(
-                i for i in self.__dict["topics"] if i["id"] == topic_id)
+                i for i in self._dict["topics"] if i["id"] == topic_id)
         except StopIteration:
             raise KeyError(topic_not_found(topic_id))
 
@@ -75,11 +75,11 @@ class DictConnection(DBConnection):
     def get_data(self, topic_id, since=None, until=None, limit=None):
         try:
             topic_d = next(
-                i for i in self.__dict["topics"] if i["id"] == topic_id)
+                i for i in self._dict["topics"] if i["id"] == topic_id)
         except StopIteration:
             raise KeyError(topic_not_found(topic_id))
         fields = topic_d["fields"]
-        data = self.__dict[topic_id]
+        data = self._dict[topic_id]
 
         if since:
             data = (i for i in data if i.get('timestamp') >= since)
