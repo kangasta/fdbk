333d582a6ecc4eaee3aa6eb30c70897c0a5d376f
Toni Kangas <toni.ar.kangas@gmail.com>
2020-04-25 23:18:30 +0300

    Add DB parameter processing to allow passing in kwargs

--- fdbk/utils/_connection.py
+++ fdbk/utils/_connection.py
@@ -1,3 +1,6 @@
+'''Utilities for creating DB connection from commandline arguments
+'''
+
 from argparse import ArgumentParser
 from importlib import import_module
 
@@ -9,19 +12,66 @@ BUILT_IN = dict(
 )
 
 
+def process_db_parameters(db_parameters):
+    '''Process commandline arguments to DB parameters
+
+    Make parameters with "=" in them into keyword arguments.
+
+    Args:
+        db_parameters: Array of commandline arguments given to DB connection
+
+    Returns:
+        (args, kwargs) tuple to expand in DB connection creation
+    '''
+    args = []
+    kwargs = {}
+
+    for i in db_parameters:
+        if "=" in i:
+            key, value = i.split("=", 1)
+            kwargs[key] = value
+        else:
+            args.append(i)
+
+    return (args, kwargs, )
+
+
 def create_db_connection(db_plugin, db_parameters):
+    '''Create DB connection from plugin name and commandline arguments
+
+    Args:
+        db_plugin: Name of the DB plugin to use.
+        db_parameters: commandline arguments for DB plugin.
+
+    Returns:
+        Created DB connection
+
+    Raises:
+        RuntimeError: Creating DB connection failed.
+    '''
     if db_plugin in BUILT_IN.keys():
         db_plugin = BUILT_IN.get(db_plugin)
 
+    args, kwargs = process_db_parameters(db_parameters)
+
     try:
         db_connection_mod = import_module(db_plugin)
-        return db_connection_mod.ConnectionClass(*db_parameters)
+        return db_connection_mod.ConnectionClass(*args, **kwargs)
     except Exception as e:
         raise RuntimeError(
             "Loading or creating fdbk DB connection failed: " + str(e))
 
 
 def get_connection_argparser(parser=None):
+    '''Create argparser for creating DB connections
+
+    Args:
+        parser: argparser to add arguments to. New parser is created by default
+            or when parser is falsy.
+
+    Returns:
+        Crated or modified parser
+    '''
     if not parser:
         parser = ArgumentParser()
 
