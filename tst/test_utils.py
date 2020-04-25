295d12941a6ce4fc8231e8c576f5795d7cb10183
Toni Kangas <toni.ar.kangas@gmail.com>
2020-04-25 23:18:30 +0300

    Add DB parameter processing to allow passing in kwargs

--- tst/test_utils.py
+++ tst/test_utils.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
-from fdbk.utils import get_connection_argparser, get_reporter_argparser
+from fdbk.utils import get_connection_argparser, get_reporter_argparser, process_db_parameters
 
-class ReporterArgparserTest(TestCase):
+class UtilsTest(TestCase):
     def test_interval_and_num_samples_default_to_none(self):
         parser = get_reporter_argparser()
         args = parser.parse_args([])
@@ -21,3 +21,17 @@ class ReporterArgparserTest(TestCase):
             args = parser.parse_args(in_)
             self.assertEqual(args.db_parameters, [])
             self.assertEqual(args.db_connection, "fdbk_dynamodb_plugin")
+
+    def test_process_db_parameters(self):
+        args, kwargs = process_db_parameters([
+            "asd",
+            "qwe=asd",
+            "zxc=qwe=asd",
+        ])
+
+        self.assertEqual(len(args), 1)
+        self.assertEqual(len(kwargs), 2)
+
+        self.assertEqual(args[0], "asd")
+        self.assertEqual(kwargs["qwe"], "asd")
+        self.assertEqual(kwargs["zxc"], "qwe=asd")
