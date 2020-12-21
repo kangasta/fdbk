02ad5d38a6233911e6cc768ccfa33db1ea13ae25
Toni Kangas <toni.ar.kangas@gmail.com>
2020-12-21 03:54:34 +0200

    Add option to store console with timestamps and stream names

--- tst/test_builtin.py
+++ tst/test_builtin.py
@@ -1,4 +1,6 @@
+import shlex
 from subprocess import CompletedProcess, PIPE, STDOUT
+from sys import executable
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
@@ -11,6 +13,24 @@ COMMON_ARGS = ['--db-connection', 'dict']
 
 
 class ParallelExecTest(TestCase):
+    def _create_topic(self, name, cmd, db_connection=None):
+        argv = [*COMMON_ARGS, 'create', '-N', name, '--start-in', '0', '--repeat-for', '0.5', '-c', *cmd]
+        with patch('sys.argv', argv):
+            main()
+
+        topics = db_connection.get_topics(template='execution')
+        self.assertEqual(len(topics), 1)
+
+        topic_d = topics[0]
+        self.assertEqual(topic_d['name'], name)
+        self.assertEqual(topic_d['metadata']['command'], cmd)
+
+        return topic_d['id']
+
+    def _execute(self, name, *args):
+        with patch('sys.argv', [*COMMON_ARGS, 'exec', '-N', name, *args]):
+            main()
+
     @patch('builtins.print')
     @patch('fdbk.builtin.parallel_exec._should_repeat', side_effect=[True, True, False])
     @patch('fdbk.builtin.parallel_exec.run')
@@ -18,30 +38,22 @@ class ParallelExecTest(TestCase):
     def test_main(self, create_mock, run_mock, repeat_mock, print_mock):
         C = create_mock.return_value
 
-        # Create topic
         name = 'Test execution'
         cmd = ['sleep', '5']
-        with patch('sys.argv', [*COMMON_ARGS, 'create', '-N', name, '--start-in', 0, '-c', *cmd]):
-            main()
-
-        topics = C.get_topics(template='execution')
-        self.assertEqual(len(topics), 1)
-
-        topic_d = topics[0]
-        topic_id = topic_d['id']
-        self.assertEqual(topic_d['name'], name)
-        self.assertEqual(topic_d['metadata']['command'], cmd)
+        topic_id = self._create_topic(name, cmd, C)
 
         # Execute
         process = CompletedProcess(cmd, 0, b'asd')
         run_mock.return_value = process
 
-        with patch('sys.argv', [*COMMON_ARGS, 'exec', '-N', name]):
-            main()
+        self._execute(name)
 
         run_mock.assert_called_with(cmd, stderr=STDOUT, stdout=PIPE)
         self.assertEqual(len(run_mock.mock_calls), 2)
         self.assertEqual(len(C.get_data(topic_id)), 2)
+
+        console = C.get_data(topic_id)[0]["output"]
+        self.assertIn('+ sleep 5', console)
 
     @patch('builtins.exit')
     @patch('builtins.print')
@@ -55,3 +67,36 @@ class ParallelExecTest(TestCase):
             main()
 
         exit_mock.assert_called_with(TOPIC_CREATION_FAILED)
+
+    @patch('builtins.print')
+    @patch('fdbk.builtin.parallel_exec.create_db_connection', return_value=DictConnection())
+    def test_json_console(self, create_mock, print_mock):
+        C = create_mock.return_value
+
+        name = 'Test execution'
+        cmd = [
+            executable or 'python',
+            '-c',
+            'import sys, time;'
+            'print("out");'
+            'time.sleep(0.5);'
+            'print("err", file=sys.stderr);'
+        ]
+
+        topic_id = self._create_topic(name, cmd, C)
+
+        self._execute(name, '--console-as-json')
+
+        self.assertEqual(len(C.get_data(topic_id)), 1)
+        console = C.get_data(topic_id)[0]['output']
+        self.assertEqual(len(console), 3)
+        self.assertEqual(shlex.split(console[0]['text']), cmd)
+
+        for stream, text in [
+            ('stdout', 'out'),
+            ('stderr', 'err'),
+        ]:
+            msg = next(i for i in console if i['stream'] == stream)
+
+            self.assertEqual(msg['stream'], stream, f'Stream: {stream}')
+            self.assertEqual(msg['text'], text, f'Stream: {stream}')
